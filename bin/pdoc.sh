075965c0da0f2ac608ddaa7eea9949083976123f
Gunn <gunn.jungpaibul@nrel.gov>
2023-12-06 01:50:20 -0500

    changed pdoc.sh to include creation of a new redirector index.html file

--- bin/pdoc.sh
+++ bin/pdoc.sh
@@ -1,1 +1,6 @@
 pdoc --html hero --force --template-dir ./pdoc/templates --output-dir docs
+
+# Creates index.html at the root, and places a redirect inside to the nested index.html
+# - for Github Pages to be able to find the entry point
+html_content='<meta http-equiv="refresh" content="0; url=./hero/index.html" />'
+echo "$html_content" > "./docs/index.html"
