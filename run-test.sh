42803536dd54a9dcfbbb97ddfc8ffed335f05d35
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-06-17 21:21:43 -0400

    Wires up resilient session from ApiBase to children

--- run-test.sh
+++ run-test.sh
@@ -2,12 +2,12 @@ export HERO_ENV="dev"
 export HERO_PROJECT="aeroportal-app"
 export HERO_CLIENT_ID="REDACTED_CLIENT_ID"
 export HERO_CLIENT_SECRET="REDACTED_CLIENT_SECRET"
-export HERO_RESILIENT_SESSION=true
+# export HERO_RESILIENT_SESSION=true
 
-poetry run pytest
+poetry run pytest -s
 
 unset HERO_ENV
 unset HERO_PROJECT
 unset HERO_CLIENT_ID
 unset HERO_CLIENT_SECRET
-unset HERO_RESILIENT_SESSION
+# unset HERO_RESILIENT_SESSION
